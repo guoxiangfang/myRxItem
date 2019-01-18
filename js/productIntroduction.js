9ab4e9018496b1758ac5139842b7bfbe6e7407c4
alikeke <1943847274@qq.com>
2019-01-18 18:51:55 +0800

    20190118  18:51

--- js/productIntroduction.js
+++ js/productIntroduction.js
@@ -3,6 +3,14 @@ $(function(){
     window.onresize = function(){
         navbarIphoneFun();
     }
+    $(".navbar-productMain ul li").on("click",function(){
+        var index=$(this).index();
+        $(this).addClass("active").siblings().removeClass("active");
+        $(".productContent .productInfo").eq(index).show().siblings().hide();
+    })
+    $('#myCarousel').carousel({
+        interval: 3000
+    });
 })
 function navbarIphoneFun() {
     if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
